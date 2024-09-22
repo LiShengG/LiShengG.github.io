9f15602df897954e95aff2e0650905651041c4e1
LiShengG <769203436@qq.com>
2024-09-22 10:43:56 +0800

    秋日日记

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -131,12 +131,12 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       link: 'https://github.com/LiShengG', // 可选的
     },
 
-    // 博主信息 (显示在首页侧边栏)
-    blogger: {
-      avatar: '/img/sheng.png',
-      name: 'lisheng',
-      // slogan: 'aaa',
-    },
+    // // 博主信息 (显示在首页侧边栏)
+    // blogger: {
+    //   avatar: '/docs/img/li2.png',
+    //   name: 'lisheng',
+    //   // slogan: 'aaa',
+    // },
 
     // 社交图标 (显示于博主信息栏和页脚栏。内置图标：https://doc.xugaoyi.com/pages/a20ce8/#social)
     social: {
---
+++ docs/.vuepress/dist/assets/js/130.d5d9ce97.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[130],{358:function(n,w,o){}}]);
